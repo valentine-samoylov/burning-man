1d84f05dec7a096b4a9a05be9174e072195a5228
Valentine Samoylov <valentine.samoylov@gmail.com>
2020-06-25 18:34:49 +0300

    index.kit, header.kit, script.js and style.scss files are changed.
    1. JS works with fles only by ID;
    3. Some variables names in JS file changed;
    2. Class names changed:
        __title -> __headline;
        __subheader -> __subheadline.

--- #source/js/script.js
+++ #source/js/script.js
@@ -1,20 +1,20 @@
 	//<Fixed header>===============================================================================
-	let header = $(".header");
-	let intro = $("#main-screen");
-	let introH = intro.innerHeight();
+	let header = $("#header");
+	let mainScreen = $("#main-screen");
+	let mainScreenH = mainScreen.innerHeight();
 	let scrollPos = $(window).scrollTop();
 
-	checkScroll(scrollPos, introH);
+	checkScroll(scrollPos, mainScreenH);
 
 	$(window).on("scroll resize", function () {
-		introH = intro.innerHeight() - 70;
+		mainScreenH = mainScreen.innerHeight() - 70;
 		scrollPos = $(this).scrollTop();
 
-		checkScroll(scrollPos, introH);
+		checkScroll(scrollPos, mainScreenH);
 	});
 
-	function checkScroll(scrollPos, introH) {
-		if (scrollPos > introH) {
+	function checkScroll(scrollPos, mainScreenH) {
+		if (scrollPos > mainScreenH) {
 			header.addClass("fixed");
 		} else {
 			header.removeClass("fixed");
@@ -30,9 +30,6 @@
 		let blockId = $this.data("scroll");
 		let blockOffset = $(blockId).offset().top;
 
-		$(".menu__link").removeClass("active");
-		$this.addClass("active");
-
 		$("html, body").animate(
 			{
 				scrollTop: blockOffset - 69,
@@ -43,14 +40,14 @@
 	//</Smooth scroll>=============================================================================
 
 	//<Icon menu button (burger menu)>=============================================================
-	$(".icon-menu").click(function (event) {
+	$("#js-icon-menu").click(function (event) {
 		$(this).toggleClass("active");
-		$(".menu__list").toggleClass("active");
+		$("#js-menu__list").toggleClass("active");
 		$("body").toggleClass("lock");
 	});
 
 	$("a").click(function (event) {
-		$(".icon-menu, .menu__list, .menu__link").removeClass("active");
+		$("#js-icon-menu, #js-menu__list").removeClass("active");
 		$("body").removeClass("lock");
 	});
 	//</Icon menu button (burger menu)>============================================================
--- js/main.js
+++ js/main.js
@@ -1,22 +1,22 @@
 
 $(document).ready(function(){
 	//<Fixed header>===============================================================================
-	let header = $(".header");
-	let intro = $("#main-screen");
-	let introH = intro.innerHeight();
+	let header = $("#header");
+	let mainScreen = $("#main-screen");
+	let mainScreenH = mainScreen.innerHeight();
 	let scrollPos = $(window).scrollTop();
 
-	checkScroll(scrollPos, introH);
+	checkScroll(scrollPos, mainScreenH);
 
 	$(window).on("scroll resize", function () {
-		introH = intro.innerHeight() - 70;
+		mainScreenH = mainScreen.innerHeight() - 70;
 		scrollPos = $(this).scrollTop();
 
-		checkScroll(scrollPos, introH);
+		checkScroll(scrollPos, mainScreenH);
 	});
 
-	function checkScroll(scrollPos, introH) {
-		if (scrollPos > introH) {
+	function checkScroll(scrollPos, mainScreenH) {
+		if (scrollPos > mainScreenH) {
 			header.addClass("fixed");
 		} else {
 			header.removeClass("fixed");
@@ -32,9 +32,6 @@ $(document).ready(function(){
 		let blockId = $this.data("scroll");
 		let blockOffset = $(blockId).offset().top;
 
-		$(".menu__link").removeClass("active");
-		$this.addClass("active");
-
 		$("html, body").animate(
 			{
 				scrollTop: blockOffset - 69,
@@ -45,14 +42,14 @@ $(document).ready(function(){
 	//</Smooth scroll>=============================================================================
 
 	//<Icon menu button (burger menu)>=============================================================
-	$(".icon-menu").click(function (event) {
+	$("#js-icon-menu").click(function (event) {
 		$(this).toggleClass("active");
-		$(".menu__list").toggleClass("active");
+		$("#js-menu__list").toggleClass("active");
 		$("body").toggleClass("lock");
 	});
 
 	$("a").click(function (event) {
-		$(".icon-menu, .menu__list, .menu__link").removeClass("active");
+		$("#js-icon-menu, #js-menu__list").removeClass("active");
 		$("body").removeClass("lock");
 	});
 	//</Icon menu button (burger menu)>============================================================
